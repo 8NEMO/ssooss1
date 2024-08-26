cc84c59384f6954b61c70064d7bf8083a13d86bc
8NEMO <accidentxxxx@gmail.com>
2024-08-26 14:29:35 +0500

    екрпе

--- finall/src/main/java/searchengine/Application.java
+++ finall/src/main/java/searchengine/Application.java
@@ -1,5 +1,8 @@
 package searchengine;
 
+
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
